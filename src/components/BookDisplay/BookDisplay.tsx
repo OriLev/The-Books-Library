ef32c3cc9a122127f15309acf869317178cfccdf
OriLev <orilevitan2@gmail.com>
2018-04-20 18:08:17 +0300

    add book function buttons

--- src/components/BookDisplay/BookDisplay.tsx
+++ src/components/BookDisplay/BookDisplay.tsx
@@ -2,15 +2,21 @@ import * as React from 'react';
 import { Book } from '../../stores/BooksStore';
 import './BookDisplay.css';
 
-export const BookDisplay = ({ book }: {book: Book}) => {
+export const BookDisplay = ({ book }: { book: Book }) => {
     const { title, publicationDate, author } = book;
     return (
         <div className="bookDisplay">
-            <h5 className="bookDisplay__field bookDisplay__field--date"><i>{publicationDate}</i></h5>
-            <h3 className="bookDisplay__field bookDisplay__field--title">{title}</h3>
-            <span className="bookDisplay__field"><i>by</i></span>
-            <h5 className="bookDisplay__field bookDisplay__field--author">{author}</h5>
-            {/* <span className="bookDisplay__field">{coverImgURL}</span> */}
+            <div className="bookFunctions">
+                <input className="pressable bookFunctions__function bookFunctions__function--edit" type="button"/>
+                <input className="pressable bookFunctions__function bookFunctions__function--delete" type="button"/>
+            </div>
+            <div className="bookFields">
+                <h5 className="bookFields__field bookFields__field--date"><i>{publicationDate}</i></h5>
+                <h3 className="bookFields__field bookFields__field--title">{title}</h3>
+                <span className="bookFields__field"><i>by</i></span>
+                <h5 className="bookFields__field bookFields__field--author">{author}</h5>
+                {/* <span className="bookFields__field">{coverImgURL}</span> */}
+            </div>
         </div>
     );
 };
